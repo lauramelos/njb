52cb5bdb708161a16866bbdc77acf4eba97bd5d4
Laura Melo <lauramelos@gmail.com>
2025-06-25 19:11:33 +0100

    create child theme

---
+++ themes/njb/functions.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * njb functions and definitions.
+ *
+ * @link https://developer.wordpress.org/themes/basics/theme-functions/
+ *
+ * @package WordPress
+ * @subpackage Twenty_Twenty_Five
+ * @since Twenty Twenty-Five 1.0
+ */
