e92a3b225c7efdd0826cc011fdddc5f72a6d4bb1
Laura Melo <lauramelos@gmail.com>
2025-07-02 16:39:28 +0100

    Change modal to interactive

--- plugins/njb-blocks/src/modal/edit.js
+++ plugins/njb-blocks/src/modal/edit.js
@@ -14,24 +14,22 @@ import { __ } from '@wordpress/i18n';
 import { useBlockProps } from '@wordpress/block-editor';
 
 /**
- * Lets webpack process CSS, SASS or SCSS files referenced in JavaScript files.
- * Those files can contain any CSS code that gets applied to the editor.
- *
- * @see https://www.npmjs.com/package/@wordpress/scripts#using-css
- */
-import './editor.scss';
-
-/**
  * The edit function describes the structure of your block in the context of the
  * editor. This represents what the editor will render when the block is used.
  *
  * @see https://developer.wordpress.org/block-editor/reference-guides/block-api/block-edit-save/#edit
  *
+ * @param {Object}   props               Properties passed to the function.
+ * @param {Object}   props.attributes    Available block attributes.
+ * @param {Function} props.setAttributes Function that updates individual attributes.
+ *
  * @return {Element} Element to render.
  */
-export default function Edit() {
+export default function Edit( { attributes, setAttributes } ) {
+	const blockProps = useBlockProps();
+	
 	return (
-		<p { ...useBlockProps() }>
+		<p { ...blockProps }>
 			{ __( 'Modal – hello from the editor!', 'modal' ) }
 		</p>
 	);
