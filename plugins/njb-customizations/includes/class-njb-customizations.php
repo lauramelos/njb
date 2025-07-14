8c60172f0035ecefe16fc167916ab9a204bd6f28
Laura Melo <lauramelos@gmail.com>
2025-07-14 13:54:05 +0100

    Add Whatsapp custom checkout field

--- plugins/njb-customizations/includes/class-njb-customizations.php
+++ plugins/njb-customizations/includes/class-njb-customizations.php
@@ -3,8 +3,8 @@ class NJB_Customizations {
     public function __construct() {
         // Initialization code here
        add_filter( 'excerpt_more', array( $this, 'custom_excerpt_more' ) );
-       add_filter( 'excerpt_length',array( $this, 'custom_excerpt_length' ) );
-
+       add_filter( 'excerpt_length', array( $this, 'custom_excerpt_length' ) );
+       add_action( 'woocommerce_init', array( __CLASS__, 'add_whatsapp_number' ) );
     }
 
     public function activate() {
@@ -36,5 +36,25 @@ class NJB_Customizations {
    
     public function custom_excerpt_length( $more ) {
         return 15;
+    }
+
+
+    public static  function add_whatsapp_number () {
+        woocommerce_register_additional_checkout_field(
+            array(
+                'id'            => 'nbj/whatsapp_number',
+                'type'          => 'text',
+                'label'         => 'WhatsApp Number',
+                'location'      => 'contact',
+                'required'      => true,
+                'attributes'    => array(
+                    'autocomplete'     => 'whatsapp_number',
+                    'aria-describedby' => 'WhatsApp Number',
+                    'aria-label'       => 'WhatsApp Number label',
+                    'pattern' => '\+?[0-9\s\-\(\)]{7,15}', // A phone number pattern allowing 7 to 15 digits with optional +, spaces, dashes, and parentheses.					'title'            => 'Title to show on hover',
+                    'data-custom'      => 'custom data',
+                ),
+            ),
+        );
     }
 }
