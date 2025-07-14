e145e5952732b00d3f480ed9f6b0ef4fea5ac238
Laura Melo <lauramelos@gmail.com>
2025-07-14 15:49:08 +0100

    Fix Modal

--- plugins/njb-blocks/src/modal/render.php
+++ plugins/njb-blocks/src/modal/render.php
@@ -1,32 +1,50 @@
-<?php  wp_interactivity_state( 'create-block', array()); ?>
+<?php  wp_interactivity_state( 'create-block', array( 'isOpen' => false )); ?>
 
 <div
-    <?php echo get_block_wrapper_attributes(  array( 'isOpen' => false ) ); ?>
+    <?php echo get_block_wrapper_attributes(); ?>
     data-wp-interactive="create-block" 
-    <?php echo wp_interactivity_data_wp_context( array( 'isOpen' => false ) ); ?>
-        data-wp-on-document--keydown="callbacks.handleEscClose"
-        data-wp-on-document--click="callbacks.handleClickOutside"
+    data-wp-on-document--keydown="callbacks.handleEscClose"
+    data-wp-on-document--click="callbacks.handleClickOutside"
     >
     <?php if ( ! empty( $content ) ) { 
         $button_text = ! empty( $attributes['buttonText'] ) ? $attributes['buttonText'] : __( 'Show Modal', 'njb-blocks' );
         ?>
-        <button class="wp-element-button js-show-modal " data-wp-on--click="actions.openModal"> 
-            <?php echo esc_html( $button_text ); ?>
-        </button>
         <div class="container">
-        <div class="modal js-modal" data-wp-class--modal--opened="context.isOpen">
-            <div class="modal__overlay"></div>
-            <div class="modal__content">
-                <button class="modal__close js-close-modal" data-wp-on--click="actions.closeModal">X
-                    <i class="bx bx-x-circle"></i>
-                </button>
-                <?php echo $content; ?>
-                <div class="modal__action">
-                    <button class="btn js-close-modal" data-wp-on--click="actions.closeModal">Close</button>
+            <div class="modal js-modal" data-wp-class--modal--opened="state.isOpen">
+                <div class="modal__overlay"></div>
+                <div class="modal__content">
+                    <button class="modal__close js-close-modal" data-wp-on--click="actions.closeModal">X
+                        <i class="bx bx-x-circle"></i>
+                    </button>
+                    <?php echo $content; ?>
+                    <div class="modal__action">
+                        <button class="btn js-close-modal" data-wp-on--click="actions.closeModal">Close</button>
+                    </div>
                 </div>
-            </div>
 
             </div>
          </div>
     <?php } ?>
 </div>
+
+<?php
+function my_custom_render( $block_content, $block ) {
+
+    if ( $block['blockName'] !== 'core/button' ) {
+        return $block_content;
+    }
+
+    $p = new WP_HTML_Tag_Processor( $block_content );
+
+    if ( ! $p->next_tag( array( 'class_name' => 'js-show-modal' ) ) ) {
+        return $block_content;
+    }
+
+    $p->set_attribute( 'data-wp-interactive', 'create-block' );
+    $p->set_attribute( 'data-wp-on--click', 'core/navigation::actions.closeMenuOnClick' );
+    $p->set_attribute( 'data-wp-on--click', 'actions.openModal' );
+
+    return $p->get_updated_html();
+}
+
+add_filter( 'render_block', 'my_custom_render', 10, 2 );
--- plugins/njb-blocks/src/modal/view.js
+++ plugins/njb-blocks/src/modal/view.js
@@ -1,45 +1,48 @@
 /**
  * WordPress dependencies
  */
-import { store, getContext } from '@wordpress/interactivity';
-
+import { store } from '@wordpress/interactivity';
 const { state } = store( 'create-block', {
 	state: {
 		isOpen: false,
 	},
 	actions: {
 		openModal() {
-			const context = getContext();
-			context.isOpen = ! context.isOpen;
+			//const context = getContext();
+			state.isOpen = ! state.isOpen;
 		},
 		closeModal() {
-			const context = getContext();
-			context.isOpen = false;
+			//const context = getContext();
+			state.isOpen = false;
+			console.log( `Is closed: ${ state.isOpen }` );
 		},
 		handleEscClose(e) {
 			if ( e.key === 'Escape' || e.keyCode === 27 ) {
-				const context = getContext();
-				context.isOpen = false;
+				//const context = getContext();
+				state.isOpen = false;
+				console.log( `Modal closed with ESC` );
 			}
 		}
 	},
 	callbacks: {
 		handleEscClose(e) {
 			if ( e.key === 'Escape' || e.keyCode === 27 ) {
-				const context = getContext();
-				context.isOpen = false;
+				//const context = getContext();
+				state.isOpen = false;
+				console.log( `Modal closed with ESC` );
 		}
 		},
 		handleClickOutside(event) {
-			const context = getContext();
-			if ( ! context.isOpen ) {
+			//const context = getContext();
+			if ( ! state.isOpen ) {
 				return;
 			}
 
-			if ( !event.target.closest('.modal__content') &&
-			  	!event.target.closest('.js-show-modal')
+			if ( ! event.target.closest('.modal__content') &&
+			  	! event.target.closest('.js-show-modal')
 			) {
-			  context.isOpen = false;
+				state.isOpen = false;
+				console.log( `Modal closed by clicking outside` );
 			}
 		}
 	}
